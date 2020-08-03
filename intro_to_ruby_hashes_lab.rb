c0b88bd23df1d5e109af3281a5284524000df540
Payton Gray <paytongray18@gmail.com>
2020-08-03 19:50:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,7 @@ end
 
 def monopoly_with_second_tier
  base_hash = {
-    :railroads => {pieces: "4"}
-        :rent_in_dollars => {}
-        :names => {}
-      
+    :railroads => {pieces "4"}
   }
 
 
